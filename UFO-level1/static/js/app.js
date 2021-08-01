46539a348383a2abfa31df5ef9a67a2044fd23ed
Karen Gutierrez <kareng018@berkeley.edu>
2021-08-01 13:20:32 -0700

    seyup fpr table without filtering

--- UFO-level1/static/js/app.js
+++ UFO-level1/static/js/app.js
@@ -1,11 +1,21 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
 
 // YOUR CODE HERE!
 function submitHandler(){
     d3.event.preventDefault();
     var inputElement = d3.select("#datetime");
     var inputValue = String(inputElement.property("value"));
+    tbody.html("");
+
+    tableData.forEach((sighting) => {
+        var row = tbody.append("tr");
+        Object.values(sighting).forEach((values) => {
+          var cell = row.append("td");
+          cell.text(values);
+        });
+      });
 };
 
 d3.select("#filter-btn").on("click", submitHandler);
