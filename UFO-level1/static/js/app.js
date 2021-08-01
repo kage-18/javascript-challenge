596aad35c3f9933c9938eb0e2cbd8d633090f0f0
Karen Gutierrez <kareng018@berkeley.edu>
2021-08-01 13:16:42 -0700

    variables for handler set

--- UFO-level1/static/js/app.js
+++ UFO-level1/static/js/app.js
@@ -3,7 +3,9 @@ var tableData = data;
 
 // YOUR CODE HERE!
 function submitHandler(){
-
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputValue = String(inputElement.property("value"));
 };
 
 d3.select("#filter-btn").on("click", submitHandler);
